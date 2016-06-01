b1ce1797e21f18f6fe950d4c6a8cd7b6b963ab94
Lisa <misslisazhou@gmail.com>
2016-06-01 00:22:31 -1000

    create object to count elements and attributes

--- js/dom_cloud.js
+++ js/dom_cloud.js
@@ -6,22 +6,6 @@ window.onload = function(){
   //empty array to hold all elements and attributes
   var everythingArr = [];
 
-  //test to display child node elements and attributes
-  var docFile = document.head;
-  console.log(document.getElementsByTagName("head"));
-  console.log(docFile); //return node requested (and html code)
-  console.log(docFile.nodeName); //.nodeName --- return string name of node (element name)
-  console.log(docFile.children.length); //.children.length --- return # of children in node
-  console.log(docFile.children[0]); //[i] --- return childNodes requested (and html code)
-  console.log(docFile.children[0].nodeName); //.nodeName --- return string name of first child
-  console.log(docFile.children[1].nodeName);
-  console.log(docFile.children[2].nodeName);
-  console.log(docFile.children[0].attributes.length); //.attributes.length --- returns whether child has any attributes (if length !== undefined)
-  console.log(docFile.children[0].attributes[0].name); //attributes[0].name --- return attribute name of children
-  console.log(docFile.children[1].attributes[0].name);
-  console.log(docFile.children[1].attributes[1].name);
-  console.log(docFile.children[1].attributes[2].name);
-
   //recursive function to traverse DOM and push elements and attributes into empty array
   function traverseDOM(node) {
     everythingArr.push(node.nodeName);
@@ -38,7 +22,19 @@ window.onload = function(){
       everythingArr.push(node.attributes[j].name);
     }
   }
-
+  //invoke recursive function with document node
   traverseDOM(theDoc);
   console.log(everythingArr);
+
+  //create object to count number of elements and attributes
+  var counterObj = {};
+  for (var k = 0; k < everythingArr.length; k++) {
+    var key = everythingArr[k];
+    if (counterObj[key]){
+      counterObj[key]++;
+    }else{
+      counterObj[key] = 1;
+    }
+  }
+  console.log(counterObj);
 };
